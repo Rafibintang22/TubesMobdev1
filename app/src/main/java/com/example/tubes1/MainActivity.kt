47dae4b8aabc5cd5885520b8b657c71fed5f66f7
Rafibintang22 <rafibintang26.rb@gmail.com>
2023-11-05 23:58:09 +0700

    update toolbar

--- app/src/main/java/com/example/tubes1/MainActivity.kt
+++ app/src/main/java/com/example/tubes1/MainActivity.kt
@@ -6,12 +6,11 @@ import com.example.tubes1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(this.layoutInflater)
-        val toolbar = binding.toolbar
-        setSupportActionBar(toolbar)
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+
         setContentView(this.binding.root)
     }
 }
