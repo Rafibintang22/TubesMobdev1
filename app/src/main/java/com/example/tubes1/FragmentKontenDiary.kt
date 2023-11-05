3e3fe1be0cdd3cdf098435f838bc5c893559d2b6
Rafibintang22 <rafibintang26.rb@gmail.com>
2023-11-05 23:41:38 +0700

    Update : (FragmentKOntenDiary)

--- app/src/main/java/com/example/tubes1/FragmentKontenDiary.kt
+++ app/src/main/java/com/example/tubes1/FragmentKontenDiary.kt
@@ -17,6 +17,21 @@ class FragmentKontenDiary : Fragment() {
     ): View? {
         this.binding = FragmentKontenDiaryBinding.inflate(inflater, container, false)
 
+        val buttonBack = binding.btnBack
+        buttonBack.setOnClickListener {
+            pindahkeFragment(FragmentHome())
+        }
         return this.binding.root
+
+        return this.binding.root
+    }
+
+    fun pindahkeFragment(fragmentBaru : Fragment){
+        val mainActivity = activity as MainActivity
+
+        val fragmentTransaction = mainActivity.supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragmentBaru)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
