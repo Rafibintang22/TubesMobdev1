f2cadc8325cabc5013e6582d1e116b98ef652a99
Vico Pratama <135086306+guguboo@users.noreply.github.com>
2023-11-05 23:12:22 +0700

    toolbar

--- app/src/main/java/com/example/tubes1/MainActivity.kt
+++ app/src/main/java/com/example/tubes1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.tubes1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tubes1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(this.layoutInflater)
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        setContentView(this.binding.root)
     }
 }
