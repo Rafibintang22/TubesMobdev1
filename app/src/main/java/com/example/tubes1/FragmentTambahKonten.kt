c0a45815dd8e733441dffbd89fe2c1ebdb9990ad
madthecreator <ahmadrizalk7@gmail.com>
2023-11-06 22:28:28 +0700

    update:menambahkan fungsi upload foto

--- app/src/main/java/com/example/tubes1/FragmentTambahKonten.kt
+++ app/src/main/java/com/example/tubes1/FragmentTambahKonten.kt
@@ -1,14 +1,23 @@
 package com.example.tubes1
 
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.tubes1.databinding.FragmentTambahKontenBinding
 
 class FragmentTambahKonten : Fragment() {
     lateinit var binding: FragmentTambahKontenBinding
+    lateinit var intentLauncher: ActivityResultLauncher<Intent>
+    var imageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +30,26 @@ class FragmentTambahKonten : Fragment() {
         buttonBack.setOnClickListener {
             pindahkeFragment(FragmentHome())
         }
+
+        val buttonUpload = binding.uploadButton
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "My Image")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Image taken from my app")
+        this.imageUri = requireActivity().contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        this.intentLauncher = this.registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){ result ->
+            if(result.resultCode == AppCompatActivity.RESULT_OK){
+                this.binding.image.setImageURI(imageUri)
+            }
+        }
+
+        buttonUpload.setOnClickListener{
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            this.intentLauncher.launch(takePictureIntent)
+        }
+        
         return this.binding.root
     }
 
